4474fa7d55a1c9c2c02e555f9d71456f2847f442
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-03-04 22:06:36 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import { connectDb } from '../utils/db.js';
 import {config} from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 config();
 
 const app=express();
 
 connectDb();
+
+app.get('/',(req,res)=>{
+    res.send("api is working")
+})
+
+app.use('/api/v1/user',userRoutes)
 
 app.listen(process.env.PORT,()=>{
     console.log("server is running on 4000!")
