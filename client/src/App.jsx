12a986563f34005ff9c50437d39d6355060a2fc6
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-03-04 17:20:45 +0530

    creates pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
-
   return (
-    <>
-   <div className="text-3xl text-blue-900 hover:text-slate-500">app</div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
