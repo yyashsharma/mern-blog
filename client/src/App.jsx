e05fe250b3bf13bdef5842e84e4988ec33a65333
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-03-04 19:12:45 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
