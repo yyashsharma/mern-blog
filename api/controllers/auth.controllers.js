1548622fb29f2788321713a68e6913c4ba42c0de
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-03-05 18:44:38 +0530

    create sign api route

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,19 +1,21 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import { generateToken } from "../utils/generateAuthToken.js";
 
 
-export const signup = async (req, res,next) => {
+
+export const signup = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
 
         if (!username || !email || !password || username === '' || email === '' || password === '') {
-            next(errorHandler(400,'Invalid details'))
+            return next(errorHandler(400, 'Invalid details'))
         }
 
         const userFound = await User.findOne({ email });
         if (userFound) {
-            return res.status(400).json({ success:false,message: "User Already Exist" });
+            return next(errorHandler(400, "User Already Exist"))
         }
 
         const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -25,9 +27,40 @@ export const signup = async (req, res,next) => {
         })
         const saved = await user.save();
 
-        res.status(201).json({ success:true,message: "User Registered Successfully" })
+        res.status(201).json({ success: true, message: "User Registered Successfully" })
 
     } catch (error) {
-       next(error)
+        next(error)
+    }
+}
+
+export const signin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password || email === '' || password === '') {
+            return next(errorHandler(400, 'Invalid details'))
+        }
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "User Not Found"))
+        }
+
+        const comparedPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!comparedPassword) {
+            return next(errorHandler(404, "Invalid Credentials"))
+        }
+
+        const token = generateToken({ validUser });
+
+        const { password: pass, ...rest } = validUser._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json({ success: true, message: "User Logged in Successfully",rest })
+
+    } catch (error) {
+        next(error)
     }
 }
---
+++ api/utils/generateAuthToken.js
@@ -0,0 +1,6 @@
+import jwt from 'jsonwebtoken'
+
+export const generateToken = ({ validUser }) => {
+    const token = jwt.sign({ "id": validUser._id }, process.env.JWT_ACCESS_SECRET, { expiresIn: "1d" })
+    return token;
+}
