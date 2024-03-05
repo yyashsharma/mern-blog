8bcef932a18e237dbb97cc51c2e00f64a4821e55
YashSharma001 <yashsharma.cse20@satyug.edu.in>
2024-03-05 19:17:08 +0530

    add sign in functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,103 @@
-import React from "react";
+import { Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link ,useNavigate} from "react-router-dom";
+import { toast } from "react-toastify";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const navigate=useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return toast.error("Please fill out all fields");
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/v1/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        return toast.error(data.message);
+      }
+      toast.success(data.message);
+      setLoading(false);
+      navigate('/')
+    } catch (error) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-8">
+        {/* left */}
+        <div className="flex-1">
+          <Link to={"/"} className="text-4xl font-bold dark:text-white">
+            <span className="px-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-l-full text-white">
+              Yash's
+            </span>
+            Blog
+          </Link>
+          <p className="text-md mt-5">
+            This is a Blog Project.You can create and view blogs here.
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="******"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? <Spinner size="sm" /> : "Sign In"}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span> Don't have an account?</span>
+            <Link to={"/sign-up"} className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -83,7 +83,7 @@ const SignUp = () => {
               <Label value="Your password" />
               <TextInput
                 type="password"
-                placeholder="Password"
+                placeholder="******"
                 id="password"
                 onChange={handleChange}
               />
