6b9812d9c2d491a70d921ebbb04a1bc3df22acc8
YashSharma001 <sharmayash4118@gmail.com>
2024-03-12 13:07:53 +0530

    add like functionality to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createComment, getPostComments } from "../controllers/comment.controller.js";
+import { createComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/create', verifyToken, createComment);
 
 router.get('/getPostComments/:postId', getPostComments);
+
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,7 +1,7 @@
 import { Button, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Comment from "./Comment";
 
@@ -10,6 +10,8 @@ const CommentSection = ({ postId }) => {
 
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState("");
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -56,7 +58,35 @@ const CommentSection = ({ postId }) => {
     getComments();
   }, [postId]);
 
-  console.log("comments", comments);
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/v1/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return toast.error(data.message);
+      }
+      setComments(
+        comments.map((comment) =>
+          comment._id === commentId
+            ? {
+                ...comment,
+                likes: data.comment.likes,
+                numberOfLikes: data.comment.likes.length,
+              }
+            : comment
+        )
+      );
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
   return (
     <div className="max-w-2xl mx-auto p-3 w-full">
       {currentUser ? (
@@ -115,7 +145,7 @@ const CommentSection = ({ postId }) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
